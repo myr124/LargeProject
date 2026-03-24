80e1b2d0408a159c1ba50496c6d948c67d9cc360
Eric George <ericgeo324@gmail.com>
2026-03-24 12:11:16 -0400

    added shadcn and tailwindcss

--- frontend/src/components/PageTitle.tsx
+++ frontend/src/components/PageTitle.tsx
@@ -1,6 +1,6 @@
 function PageTitle() {
     return (
-        <h1 id="title">COP 4331 MERN Stack Demo</h1>
+        <h1 className="text-3xl text-blue-500" id="title">COP 4331 MERN Stack Demo</h1>
     );
 };
 export default PageTitle;
