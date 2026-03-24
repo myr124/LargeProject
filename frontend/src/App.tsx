e43e18501f7eb90a3fb8a21cb1fc5cf6eae6c767
Eric George <ericgeo324@gmail.com>
2026-03-24 12:21:16 -0400

    vibed up boilerplate to showcase shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-
-import LoginPage from './pages/LoginPage';
-import CardPage from './pages/CardPage';
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/cards" element={<CardPage />} />
+                <Route path="/" element={<HomePage />} />
             </Routes>
         </BrowserRouter>
     );
--- frontend/src/components/PageTitle.tsx
+++
@@ -1,6 +0,0 @@
-function PageTitle() {
-    return (
-        <h1 className="text-3xl text-blue-500" id="title">COP 4331 MERN Stack Demo</h1>
-    );
-};
-export default PageTitle;
