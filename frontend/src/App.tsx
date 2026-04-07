e5168099bff6740d66a87254b6c327dc36360485
Eric George <ericgeo324@gmail.com>
2026-04-07 18:29:09 -0400

    changes to frontend - shadcn rewrite and dark mode toggle

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import HomePage from "./pages/HomePage";
+import BreadBoxd from "./pages/BreadBoxd";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<HomePage />} />
+                <Route path="/" element={<BreadBoxd />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
             </Routes>
         </BrowserRouter>
     );
